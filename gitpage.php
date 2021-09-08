5e7344f4e51188c682444ce191a084fafe52f4e7
sudharshan3 <sudharshanrampoore@gmail.com>
2021-09-08 20:15:57 +0530

    test gitpage

--- gitpage.php
+++ gitpage.php
@@ -1,1 +1,13 @@
-<h1>working git</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GITpage</title>
+</head>
+<body>
+    <h1>
+        test
+    </h1>
+</body>
+</html>

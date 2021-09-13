961ef26560a1d79a516b86c878bd2157c8af15fe
sudharshan3 <sudharshanrampoore@gmail.com>
2021-09-13 20:39:17 +0530

    test

--- index.php
+++ index.php
@@ -367,7 +367,7 @@ echo "<button class='smallbuttons btn btn-sm btn-default filter-button bg-".$col
             autoplayTimeout: 4000,
             responsive: {
                 0: {
-                    items: 2
+                    items: 1
                 },
                 900: {
                     items: 2

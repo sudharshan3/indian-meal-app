92c1b3615293d024b3403fdbab0925a4ea71f27c
sudharshan3 <sudharshanrampoore@gmail.com>
2021-09-16 10:16:15 +0530

    samosa

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                 <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/Ras Malai.jpg');">         <h6 class="slidetext">Ras Malai</h6></div>
                 <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/Rava Dosa.jpg');">         <h6 class="slidetext">Rava Dosa</h6></div>
                 <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/ROTI.jpg');">               <h6 class="slidetext">chapatti</h6></div>
-                <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/Samosas.jpg');">            <h6 class="slidetext">Samosas</h6></div>
+                <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/Samosas.jpg');">            <h6 class="slidetext">Samosa</h6></div>
                 <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/Upma.jpg');">               <h6 class="slidetext">Upma</h6></div>
                 <div class="shadow item  rounded m-3 slide_img" style="background-image: url('./assets/slider/VADA PAV.jpg');">          <h6 class="slidetext">Vada PAV</h6></div>
                 
